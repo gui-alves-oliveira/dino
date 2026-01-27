7bf7752027de24a3786aac4d2de6c61c872ae8a0
Guilherme Oliveira <g.alves.oliveira@pm.me>
2026-01-27 01:26:55 -0300

    keyboard nav

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,17 @@
 import Menu from "./components/menu";
 
 export function App() {
-  const handleClick = () => {
-    console.log("this is my client click");
-  };
-
   return (
     <div>
       <Menu>
-        <Menu.Trigger onClick={handleClick}>hello</Menu.Trigger>
+        <Menu.Trigger>hello</Menu.Trigger>
 
         <Menu.Popover>
-          <Menu.Item onClick={() => console.log("item 1")}>Item 1</Menu.Item>
-          <Menu.Item onClick={() => console.log("item 2")}>Item 2</Menu.Item>
-          <Menu.Item onClick={() => console.log("item 3")}>Item 3</Menu.Item>
+          <Menu.Item>Item 1</Menu.Item>
+          <Menu.Item>Item 2</Menu.Item>
+          <Menu.Item>Item 3</Menu.Item>
         </Menu.Popover>
       </Menu>
-
-      <button>oi</button>
     </div>
   );
 }
